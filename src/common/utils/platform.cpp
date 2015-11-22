121d32708f091b632726f3efddbbdc27e2c7c1a3
Usbuild <njuzhangqichao@gmail.com>
2015-11-22 21:34:46 +0800

    add windows support

---
+++ src/common/utils/platform.cpp
@@ -0,0 +1,43 @@
+#include "utils/platform.h"
+#ifdef WIN32
+#include <network/socket.h>
+#endif
+
+int socketclose(int fd)
+{
+#ifdef WIN32
+	return ::closesocket(fd);
+#else
+	return ::close(socket);
+#endif
+}
+
+#ifdef WIN32
+unsigned int writev(int fd, struct iovec *vec, unsigned int size)
+{
+	unsigned int s = 0;
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		int ret = ::send(fd, static_cast<char*>(vec[i].iov_base), vec[i].iov_len, 0);
+
+		if (ret == vec[i].iov_len)
+		{
+			s += ret;
+		} else if (ret >= 0)
+		{
+			s += ret;
+			return s;
+		} else
+		{
+			if (s > 0)
+			{
+				return s;
+			} else
+			{
+				return ret;
+			}
+		}
+	}
+	return s;
+}
+#endif
